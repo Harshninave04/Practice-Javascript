cec8fe4e6b37a48f1aa4915dda31bd877d9260df
Harsh Ninave <harshninave58@gmail.com>
2023-08-31 21:34:29 +0530

    Learned something about Objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,17 @@
+// object destructuring
+
+const subjects = {
+    subj1: "Mathematics",
+    subj2: "English",
+    subj3: "Physics",
+}
+
+const { subj1: Maths } = subjects;
+
+console.log(Maths);
+
+// JSON API
+
+{
+
+}
