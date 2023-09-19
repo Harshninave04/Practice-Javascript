76346793f7a5f1e5ea83c0bd263fa2f06fbb83e3
Harsh Ninave <harshninave58@gmail.com>
2023-09-19 13:02:54 +0530

    call stack

---
+++ 03_basics/04_callStack.js
@@ -0,0 +1,18 @@
+function myName() {
+    console.log("Harsh is my Name.")
+
+    function myAge() {
+        console.log("I am 19 years old.")
+
+        function lastName() {
+            console.log("Ninave is my last name.")
+        }
+
+        lastName();
+    }
+
+    myAge();
+
+}
+
+myName();
