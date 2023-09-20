4a437ed82dc6e62006cb444b57a07541952a14a4
Harsh Ninave <harshninave58@gmail.com>
2023-09-20 23:52:13 +0530

    JS manipulation

--- 07_Dom/intro.js
+++ 07_Dom/intro.js
@@ -1,3 +1,20 @@
-let headerF = document.getElementsByClassName("heading");
+// let headerF = document.getElementsByClassName("heading");
 
-headerF.innerHTML = "Harsh S. Ninave";
+// headerF.innerHTML = "Harsh S. Ninave";
+
+const div = document.createElement("div");
+console.log(div)
+
+div.setAttribute("class", "apnaDiv");
+div.setAttribute("id", "apnaId")
+
+div.style.backgroundColor = "green";
+div.style.padding = "20px"
+div.style.fontSize = "60px"
+div.style.fontWeight = "bold"
+div.style.border = "12px solid palegreen"
+div.style.borderRadius = "10px"
+const addText = document.createTextNode("Harsh I love you!!")
+div.appendChild(addText)
+
+document.body.appendChild(div)
