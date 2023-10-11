2d356a505a4ef83d8717e2f537c376f099a2739a
Harsh Ninave <harshninave58@gmail.com>
2023-10-11 22:29:13 +0530

    object literals

---
+++ 02_basics/06_object-literals.js
@@ -0,0 +1,15 @@
+let identity = {
+  first_name: "Harsh",
+  last_name: "Ninave",
+  University: "JD College of Engineering and Management",
+  Year: "Third",
+  Address: {
+    city: "Nagpur",
+    state: "Maharashtra",
+    Plot_No: 72,
+  },
+  Hobbies: ["Playing cricket", "Watching porn", "Coding"],
+};
+
+console.log(identity.first_name.toUpperCase());
+console.log(identity.Address.state.toUpperCase());
