05a97d440f641e2caa49523fed9175726075d961
Harsh Ninave <harshninave58@gmail.com>
2023-10-13 21:59:25 +0530

    loops

--- 05_iteration/03_for-of.js
+++ 05_iteration/03_for-of.js
@@ -19,3 +19,34 @@ const naav = ["Harsh", "Kunal", "Harkirat", "Dev"];
 for (let i = 0; i < naav.length; i++) {
     console.log(`Kucch youtuber ke naam ${naav[i]}`)
 }
+
+
+
+let routine = [
+  {
+    id: 1,
+    name: "Harsh",
+    email: "harsh@ninave.com",
+  },
+  {
+    id: 2,
+    name: "Dev",
+    email: "dev@mulkalwar.com",
+  },
+  {
+    id: 3,
+    name: "Yash",
+    email: "yash@ingole.com",
+  },
+];
+
+// This is for-of loop for an object literals
+for (let rotine of routine) {
+  console.log(rotine.name);
+}
+for (let rotine of routine) {
+  console.log(rotine.id);
+}
+for (let rotine of routine) {
+  console.log(rotine.email);
+}
