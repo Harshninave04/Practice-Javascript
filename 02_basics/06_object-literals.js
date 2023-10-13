945b762438dfdf24adcef86fb00b4f42a1a427ae
Harsh Ninave <harshninave58@gmail.com>
2023-10-13 21:59:09 +0530

    object literals

--- 02_basics/06_object-literals.js
+++ 02_basics/06_object-literals.js
@@ -13,3 +13,37 @@ let identity = {
 
 console.log(identity.first_name.toUpperCase());
 console.log(identity.Address.state.toUpperCase());
+console.log(identity.first_name, identity.last_name);
+console.log(identity.Hobbies[2], identity.Hobbies[1]);
+
+// Array objects -
+
+let routine = [
+  {
+    id: 1,
+    name: "Harsh",
+    email: "harsh@ninave.com",
+  },
+  {
+    id: 2,
+    name: "Dev",
+    email: "dev@mulkalwar.com",
+  },
+  {
+    id: 3,
+    name: "Yash",
+    email: "yash@ingole.com",
+  },
+];
+
+console.log(routine[2].name);
+console.log(routine[2].email);
+console.log(routine[2].id);
+
+console.log(routine[1].name);
+console.log(routine[1].email);
+console.log(routine[1].id);
+
+const routineJSON = JSON.stringify(routine);
+console.log(routineJSON);
+
