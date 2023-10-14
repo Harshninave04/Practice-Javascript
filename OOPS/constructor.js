dee9dab1cde3782953b7db580da71802356a6711
Harsh Ninave <harshninave58@gmail.com>
2023-10-14 23:55:39 +0530

    constructor function

---
+++ OOPS/constructor.js
@@ -0,0 +1,12 @@
+function person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.dob = dob;
+}
+
+const person1 = new person("Harsh", "Ninave", "4-11-2004");
+const person2 = new person("Reetik", "Bhave", "1-7-2002");
+const person3 = new person("Harsh", "Meshram", "19-8-2003");
+console.log(person1);
+console.log(person2);
+console.log(person3);
