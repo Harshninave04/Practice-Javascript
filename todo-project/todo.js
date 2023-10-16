48c91f8c387b9437b3cf011e86198d10a8f5f063
Harsh Ninave <harshninave58@gmail.com>
2023-10-16 23:31:50 +0530

    Testing

--- todo-project/todo.js
+++ todo-project/todo.js
@@ -1,3 +1,31 @@
 let element = document.createElement("li");
-element.innerText = Item1;
+element.innerHTML = "Item 2";
 document.body.appendChild(element);
+element.style.fontSize = "30px";
+
+// let lielement = document.querySelector(".items");
+// lielement.append(element);
+
+// element = document.createElement("li");
+// element.innerHTML = "Item 3";
+// document.body.appendChild(element);
+
+// element = document.createElement("li");
+// element.innerHTML = "Item 4";
+// document.body.appendChild(element);
+
+// var button = document.createElement("button");
+// button.innerText = "Click me!";
+// document.body.appendChild(button);
+// button.style.background = "yellow";
+// button.style.padding = "10px";
+let itemcount = 3;
+const btn = document.querySelector(".mytextbtn");
+btn.addEventListener("click", () => {
+  element = document.createElement("li");
+  element.innerHTML = "Item " + itemcount++;
+  document.body.appendChild(element);
+  element.style.fontSize = "30px";
+});
+
+document.querySelector("title").textContent = "My New Title";
