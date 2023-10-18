6e1092bfead0d17b4ba6c7d695b46205d74fb852
Harsh Ninave <harshninave58@gmail.com>
2023-10-18 23:16:11 +0530

    initial coundown commit

---
+++ countdown-project/script.js
@@ -0,0 +1,4 @@
+const day = document.querySelector(".days");
+const hour = document.querySelector(".hours");
+const minute = document.querySelector(".minutes");
+const second = document.querySelector(".seconds");
