cfe56883dfb401e4b0aa76cac1079a259eb830e1
Harsh Ninave <harshninave58@gmail.com>
2023-10-18 23:17:01 +0530

    waste commit

--- 07_Dom/intro.js
+++ 07_Dom/intro.js
@@ -1,20 +1,5 @@
-// let headerF = document.getElementsByClassName("heading");
-
-// headerF.innerHTML = "Harsh S. Ninave";
-
-const div = document.createElement("div");
-console.log(div)
-
-div.setAttribute("class", "apnaDiv");
-div.setAttribute("id", "apnaId")
-
-div.style.backgroundColor = "green";
-div.style.padding = "20px"
-div.style.fontSize = "60px"
-div.style.fontWeight = "bold"
-div.style.border = "12px solid palegreen"
-div.style.borderRadius = "10px"
-const addText = document.createTextNode("Harsh I love you!!")
-div.appendChild(addText)
-
-document.body.appendChild(div)
+const submitbtn = document.querySelector(".submitbtn");
+submitbtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("body").style.background = "#ccc";
+});
