0e5b92753f7af694a50349dfebe87e4072e94be5
Harsh Ninave <harshninave58@gmail.com>
2023-10-29 13:22:46 +0530

    basics

--- 01_basics/typeConversion.js
+++ 01_basics/typeConversion.js
@@ -1,8 +1,15 @@
-const number = 50;
+// to convert a string to number and vice versa
+
 const value = "50";
-console.log(number);
-console.log(typeof number);
 console.log(value);
 console.log(typeof value);
 
 console.log(parseInt(value));
+
+// to convert a number to a string
+
+let number = 50;
+// number = number.toString();
+number = String(number);
+console.log(number);
+console.log(typeof number);
