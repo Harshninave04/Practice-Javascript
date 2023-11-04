7589c7afefbcef0d5476809f45f40deeb693f9e7
Harsh Ninave <harshninave58@gmail.com>
2023-11-04 17:14:25 +0530

    for-in

--- 05_iteration/04_for-in.js
+++ 05_iteration/04_for-in.js
@@ -1,24 +1,34 @@
-// for in
+// for in - The "for...in" loop in javascript is used to iterate over the enumerable properties of an object. It is a way to loop through the keys(property names) of an object.
 
 // Object
 const language = {
-    js: "javascript",
-    py: "python",
-    cpp: "c++",
-    java: "java language for android"
-}
-
+  js: "javascript",
+  py: "python",
+  cpp: "c++",
+  java: "java language for android",
+};
 for (const key in language) {
-    console.log(`${key} shortcut is for ${language[key]}`)
+  console.log(`${key} shortcut is for ${language[key]}`);
 }
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++");
 
 var resume = {
-    about: "Harsh Ninave",
-    education: "Btech",
-    salary: "2341234",
-    city: "diamond Nagar"
-}
+  about: "Harsh Ninave",
+  education: "Btech",
+  salary: "2341234",
+  city: "diamond Nagar",
+};
 
 for (const key in resume) {
-    console.log(`The ${key} is ${resume[key]}`)
+  console.log(`${key}: ${resume[key]}`);
+}
+
+const object = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+for (let properties in object) {
+  console.log(`${properties}: ${object[properties]}`);
 }
