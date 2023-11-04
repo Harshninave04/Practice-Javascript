e0a09a5f6da23363d69b1df2fc2022e2bff1ce28
Harsh Ninave <harshninave58@gmail.com>
2023-11-04 11:55:44 +0530

    destructuring craziness!

--- 08_Destructuring/03_function-des.js
+++ 08_Destructuring/03_function-des.js
@@ -14,3 +14,64 @@ function printPersonInfo({ personName, personAge, personProfession, personCountr
   );
 }
 printPersonInfo(person);
+
+
+// Destructuring Craziness !!
+
+
+// Here data is the object
+const data = {
+  user: {
+    id: 32,
+    name: "harsh",
+    age: 19,
+    email: "harshninave58@gmail.com",
+    address: {
+      city: "Nagpur",
+      country: "India"
+    },
+    hobbies: ["Cricket", "Programming", "Watching Movies"],
+    CGPA: {
+      Year1: 9.89,
+      Year2: 9.66,
+      Year3: 9.33,
+      Year4: 9.29,
+    },
+  },
+  products: [
+    {id: 1, name: "Laptop", price: 100000 },
+    {id: 2, name: "Mobile", price: 10000 },
+    {id: 3, name: "Television", price: 1000000 },
+  ]
+}
+
+
+// Below is destructuring
+const {
+  user: {
+    id,
+    name,
+    age,
+    address: { city, country },
+    hobbies,
+    CGPA: { Year1, Year2, Year3, Year4 }
+  },
+  products: [
+    product1,
+    product2,
+    product3,
+  ]
+} = data;
+
+console.log(`Id: ${id}`)
+console.log(`Name: ${name}`)
+console.log(`Age: ${age}`)
+console.log(`Address: ${city} , ${country}`)
+console.log(`Hobbies: ${hobbies.join(", ")}`)
+console.log(`CGPA: ${Year1}`)
+console.log(`CGPA: ${Year2}`)
+console.log(`CGPA: ${Year3}`)
+console.log(`CGPA: ${Year4}`)
+console.log(`Products: ${product1.name} - ${product1.price}`)
+console.log(`Products: ${product2.name} - ${product2.price}`)
+console.log(`Products: ${product3.name} - ${product3.price}`)
