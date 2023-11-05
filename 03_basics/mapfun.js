bfb168cc6dcc99b7dad33f385b42945f27261d77
Harsh Ninave <harshninave58@gmail.com>
2023-11-05 19:09:47 +0530

    Map function

---
+++ 03_basics/mapfun.js
@@ -0,0 +1,15 @@
+// .map() - It is used to iterate over all the elements in an array, which results in a new array.
+
+let peoples = ["Harsh", "Dev", "Girish", "Pravinya", "Reetik"];
+
+let peopleName = peoples.map((person) => {
+  return person;
+});
+console.log(peopleName);
+
+// Create array with random numbers. Map over each item in that array, and multiply each item by 10
+let arr = [1, 2, 3, 4, 5, 6];
+let double = arr.map((num) => {
+  return num * 5;
+});
+console.log(double);
