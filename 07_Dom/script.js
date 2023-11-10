61b2b7648b7e659b22bcf89b781a44017c3a70b8
Harsh Ninave <harshninave58@gmail.com>
2023-11-10 23:32:15 +0530

    text manipulation

--- 07_Dom/script.js
+++ 07_Dom/script.js
@@ -1,4 +1,7 @@
 
 // const para = document.querySelector("p").innerHTML;
-const para = document.querySelector("p").textContent;
-console.log(para);
+const para = document.querySelector("p");
+para.innerHTML =
+  "My name is Harsh Ninave, I live in nagpur and my main area of focus is DevOps and cloud computing";
+para.style.fontSize = "20px";
+console.log(para.textContent);
