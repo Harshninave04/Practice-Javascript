0822cce592a6f087233dddccc9034d933307459f
Harsh Ninave <harshninave58@gmail.com>
2023-11-10 23:45:21 +0530

    DOM Manipulation

--- 07_Dom/script.js
+++ 07_Dom/script.js
@@ -1,7 +1,8 @@
-
+const headerName = document.querySelector(".headName");
+headerName.innerHTML = "<em>Harsh Ninave, DevOps Engineer</em>";
 // const para = document.querySelector("p").innerHTML;
 const para = document.querySelector("p");
 para.innerHTML =
-  "My name is Harsh Ninave, I live in nagpur and my main area of focus is DevOps and cloud computing";
+  "My name is Harsh Ninave, I live in nagpur and my main area of focus is DevOps and cloud computing. I will be making a good revolution in this field.";
 para.style.fontSize = "20px";
 console.log(para.textContent);
