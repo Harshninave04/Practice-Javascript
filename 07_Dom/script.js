19b389de886adef6ac7aceb818369c27b3d473c6
Harsh Ninave <harshninave58@gmail.com>
2023-11-11 23:37:26 +0530

    href and input value JS

--- 07_Dom/script.js
+++ 07_Dom/script.js
@@ -6,3 +6,11 @@ para.innerHTML =
   "My name is Harsh Ninave, I live in nagpur and my main area of focus is DevOps and cloud computing. I will be making a good revolution in this field.";
 para.style.fontSize = "20px";
 console.log(para.textContent);
+
+const a = document.querySelectorAll("a");
+console.log(a[0].href); // By using href we can have access to anchor links
+console.log(a[1].href);
+console.log(a[2].href);
+
+const input = document.querySelector("input");
+console.log(input.value);
