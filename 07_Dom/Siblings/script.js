516f1dd6ecc2aa8af68cc13a7cc5fc9a82c3cde8
Harsh Ninave <harshninave58@gmail.com>
2023-11-13 00:03:10 +0530

    Next & Previous Sibling Selector

--- 07_Dom/Siblings/script.js
+++ 07_Dom/Siblings/script.js
@@ -22,3 +22,15 @@ console.log((ul.children[0].innerText = "one")); // We can do this as well
 console.log(ul.children[1].getAttribute("class"));
 console.log(ul.children[2]);
 console.log(ul.children[3].getAttribute("class"));
+
+// Now let's learn about Siblings !
+// It's about how to take next sibling
+console.log(firstLi.nextElementSibling.innerText)
+console.log(firstLi.nextElementSibling.nextElementSibling.innerText)
+
+// It's about how to get previous sibling
+const fourth = document.querySelector(".fourth");
+console.log("+++++++++++++++Previous Element Selector++++++++++++++++++++++++")
+console.log(fourth.previousElementSibling.innerText)
+console.log(fourth.previousElementSibling.previousElementSibling.innerText)
+console.log(fourth.previousElementSibling.previousElementSibling.previousElementSibling.innerText)
