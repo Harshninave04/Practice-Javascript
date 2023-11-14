491d2d78af14997abcb8e2be07199aef80ccd716
Harsh Ninave <harshninave58@gmail.com>
2023-11-14 22:56:03 +0530

    DOM Create Element

---
+++ 07_Dom/DOM MANIPULATION/app.js
@@ -0,0 +1,20 @@
+// createElement()
+// appendChild()
+// append()
+// prepend()
+// insertBefore()
+
+// removeChild()
+// remove()
+
+// Creating Element ->
+
+const h1 = document.createElement("h1");
+h1.textContent = "Good Byee!!";
+h1.classList.add("greeting");
+console.log(h1);
+
+// // Append Child ->
+
+// const body = document.body;
+// body.appendChild(h1);
