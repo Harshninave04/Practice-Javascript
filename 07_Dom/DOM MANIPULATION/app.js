1d1ce009e30479d5981de01fdf68d28b1fac9a4d
Harsh Ninave <harshninave58@gmail.com>
2023-11-14 23:44:11 +0530

    append and prepend

--- 07_Dom/DOM MANIPULATION/app.js
+++ 07_Dom/DOM MANIPULATION/app.js
@@ -2,7 +2,7 @@
 // appendChild()
 // append()
 // prepend()
-// insertBefore()
+// insertBefore(what,where)
 
 // removeChild()
 // remove()
@@ -28,3 +28,17 @@ ul.appendChild(newLi);
 const firstLi = document.querySelector("li");
 // insertBefore(what,where); It'll insert the text on top of the firstLi
 ul.insertBefore(newLi, firstLi);
+
+
+
+// ++++++++++++++++++++++++++++++++++++++
+// The example of Append-
+// append is used to add elements to the last 
+// prepend add element on top 
+const newList = document.querySelector(".new-list");
+const NewLi = document.createElement("li");
+const antherLi = document.createElement("li");
+antherLi.innerText = "I'm the another li";
+NewLi.innerText = "I'm the Last li"
+newList.append(NewLi,antherLi);
+// newList.prepend(NewLi,antherLi)
