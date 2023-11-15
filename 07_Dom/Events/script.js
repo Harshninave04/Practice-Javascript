2239bf67bf493ce434d798cebcdab83b57c45b32
Harsh Ninave <harshninave58@gmail.com>
2023-11-15 20:53:59 +0530

    Event Listener

--- 07_Dom/Events/script.js
+++ 07_Dom/Events/script.js
@@ -10,6 +10,7 @@ const best = document.querySelector(".best");
 // best.addEventListener("click", function () {
 //   console.log("Yeahhh this is the actual best way to play with events.");
 // });
+// We can even pass arrow function instead of normal function.
 best.addEventListener("click", greeting);
 function greeting() {
   console.log("Yeahhh this is the actual best way to play with events.");
