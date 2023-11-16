3575d7ef0b1bec736744171b3bc1e7239dc393e6
Harsh Ninave <harshninave58@gmail.com>
2023-11-16 23:34:33 +0530

    Event Object

--- 07_Dom/Events/script.js
+++ 07_Dom/Events/script.js
@@ -15,3 +15,19 @@ best.addEventListener("click", greeting);
 function greeting() {
   console.log("Yeahhh this is the actual best way to play with events.");
 }
+
+
+// Event(e) Object
+
+const para = document.querySelector(".para");
+para.addEventListener("click", (event) => {
+  console.log(event)
+})
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(input.value)
+})
