309f5131fe7b1df10ed2f44acd58d42943c807f9
Harsh Ninave <harshninave58@gmail.com>
2023-11-19 23:42:58 +0530

    Comments added

--- Projects/Interchangeable colors/app.js
+++ Projects/Interchangeable colors/app.js
@@ -1,16 +1,26 @@
+// Select all elements with the class "btn" and store them in the btns variable
 const btns = document.querySelectorAll(".btn");
+
+// Get the body element of the document
 const body = document.body;
 
+// Iterate over each button in the btns NodeList
 btns.forEach((btn) => {
+  // Add a click event listener to each button
   btn.addEventListener("click", () => {
+    // Get the color value associated with the clicked button
     color = btn.value;
+    // Call the changeBackground function with the selected color
     changeBackground(color);
   });
 });
 
+// Define the changeBackground function that takes a color parameter
 function changeBackground(color) {
+  // Remove any existing classes from the body element
   body.className = "";
 
+  // Use a switch statement to determine which color was passed and add corresponding class to the body
   switch (color) {
     case "purple":
       body.classList.add("purple");
@@ -34,6 +44,7 @@ function changeBackground(color) {
       body.classList.add("orange");
       break;
     default:
+      // Do nothing for other cases
       break;
   }
 }
