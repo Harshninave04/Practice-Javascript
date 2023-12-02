f3a3cc9105e1377fb335813c441146cbe8c395b3
Harsh Ninave <harshninave58@gmail.com>
2023-12-02 23:56:40 +0530

    Javascript RegExp added for searching

--- Projects/Search and Highlight/app.js
+++ Projects/Search and Highlight/app.js
@@ -2,3 +2,13 @@
 
 const p = document.getElementById("p");
 const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", () => {
+  let input = document.getElementById("input").value;
+
+  if (input != "") {
+    // Regular expressions, or regex for short, are a sequence of characters that define a search pattern. They can be used to search, edit, or manipulate text and data. Regex is a powerful tool that can be used to solve a variety of problems, from finding specific words or phrases in a text to validating data.
+    let regExp = new RegExp(input, "gi");
+    p.innerHTML = p.textContent.replace(regExp, "<mark>$&</mark>");
+  }
+});
